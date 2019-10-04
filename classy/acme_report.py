c5f2ecfb35877c86fdd7d93c2162cc0e2b1c1ef9
lucguittard <lucguittard@users.noreply.github.com>
2019-10-04 16:44:30 -0600

    DS Unit 3 Sprint Challenge Revist commit

--- classy/acme_report.py
+++ classy/acme_report.py
@@ -5,32 +5,46 @@ from acme import Product
 ADJECTIVES = ['Awesome', 'Shiny', 'Impressive', 'Portable', 'Improved']
 NOUNS = ['Anvil', 'Catapult', 'Disguise', 'Mousetrap', '???']
 
+# function to generate a list of products
 def generate_products(num_products=30):
     products = []
+    price = []
+    weight = []
+    flammability = []
+    total_price = 0
+    total_weight = 0
+    total_flammability = 0
     for i in range(num_products):
-        product = sample(NOUNS, 1)
-        products.append(product)
-    return products
+        tt = Product(name = '{} {}'.format(sample(ADJECTIVES,1),
+            sample(NOUNS,1)),
+            price=randint(5,100),
+            weight=randint(5,100),
+            flammability=uniform(0,3)
+            )
+        products.append(tt.name)
+        #products.append(tt.price)
+        #products.append(tt.weight)
+        #products.append(tt.flammability)
+        total_price += tt.price
+        total_weight += tt.weight
+        total_flammability += tt.flammability
 
-def inventory_report(products):
-    Name = '{} {}'.format(sample(ADJECTIVES,1),
-        sample(NOUNS,1))
-    tt = Product(
-        name=Name,
-        price=randint(5,100),
-        weight=randint(5,100),
-        flammability=uniform(0.0,2.5)
-        )
+    return products, total_price, total_weight, total_flammability
 
-    #for i in products: come back to later
-    #For the report, you should calculate and print the following values:
-    # Average (mean) price, weight, and flammability of listed products
-    # loop over products to calculate report
+# funtion to generate an inventory report
+def inventory_report(products): #,price,weight,flammability):
+    unique_products_count = len(products)
 
-    unique_products = set(products)
-    unique_products_count = len(unique_products)
 
-    return 'There are {} unique products'.format(unique_products_count)
+    avg_price = total_price/len(products)
+    avg_weight = total_weight/len(products)
+    avg_flammability = total_flammability/len(products)
+
+    print('ACME CORPORATION OFFICIAL INVENTORY REPORT')
+    print('There are {} unique products'.format(unique_products_count))
+    print('Average price:', avg_price)
+    print('Average weight', avg_weight)
+    print('Average flammability', avg_flammability)
 
 if __name__ == '__main__':
     inventory_report(generate_products())
